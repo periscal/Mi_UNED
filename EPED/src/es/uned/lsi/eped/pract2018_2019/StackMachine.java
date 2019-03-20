9c86989b73c0f1b8e4a6513f67b5407321a1d5c4
periscal <47391317+periscal@users.noreply.github.com>
2019-03-20 02:18:02 +0100

    StackMachine completada

--- EPED/src/es/uned/lsi/eped/pract2018_2019/StackMachine.java
+++ EPED/src/es/uned/lsi/eped/pract2018_2019/StackMachine.java
@@ -3,23 +3,46 @@ package es.uned.lsi.eped.pract2018_2019;
 import es.uned.lsi.eped.DataStructures.BTree;
 import es.uned.lsi.eped.DataStructures.BTreeIF;
 import es.uned.lsi.eped.DataStructures.Queue;
+import es.uned.lsi.eped.DataStructures.QueueIF;
 import es.uned.lsi.eped.DataStructures.Stack;
 
 public class StackMachine {
 
-	Stack<Value.ValueClass> pila; /*Del Estudiante: pila */
+	Stack<Operand> pila; /*Del Estudiante: pila */
 	Queue<Value.ValueClass> colaPostOrden;
-	
+
 	public StackMachine() {
-		pila = new Stack<Value.ValueClass>();
-		colaPostOrden = new Queue<Value.ValueClass>();
+		pila = new Stack<>();
+		colaPostOrden = new Queue<>();
 	}
-	
+
 	public Operand execute(SynTree syn) {
 		BTree<Node> b=syn.getSynTree();
-		pila.push(b.getLeftChild());
-		//while (colaPostOrden.)
-		return (new Operand("0"));
+		postOrden(syn.getSynTree());
+		return pila.getTop()/*(new Operand("0"))*/;
 	}
-	
+
+	/* Recorrido del arbol en postorden */
+	public void postOrden(BTreeIF<Node> t) {
+		if ( !t.isEmpty() ) {
+			if ( t.getLeftChild() != null ) { postOrden(t.getLeftChild()); }
+			if ( t.getRightChild() != null ) { postOrden(t.getRightChild()); }
+
+			Node nodo = t.getRoot();
+			if (nodo.getNodeType() == Node.NodeType.OPERAND) {
+				pila.push((Operand) nodo);
+			}else if ( nodo.getNodeType() == Node.NodeType.OPERATOR ) {
+				Operand primerOperando = pila.getTop();
+				pila.pop();
+				Operator.OperatorType tipoOperador = ((Operator)nodo).getOperatorType();
+				switch(tipoOperador) {
+					case ADD: pila.getTop().add(primerOperando); break;
+					case SUB: pila.getTop().sub(primerOperando); break;
+					case MULT:pila.getTop().mult(primerOperando);break;
+				}
+				
+			}
+		}
+	}
+
 }
