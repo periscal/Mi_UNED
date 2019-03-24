292946e1bcc627c1a2392e94119e4ffef95ef319
juan <juan@juan-System-Product-Name>
2019-03-24 01:23:58 +0100

    Fragmentacion numero de ValueSeq

--- EPED/src/es/uned/lsi/eped/pract2018_2019/ValueSeq.java
+++ EPED/src/es/uned/lsi/eped/pract2018_2019/ValueSeq.java
@@ -1,9 +1,15 @@
 package es.uned.lsi.eped.pract2018_2019;
 
-public class ValueSeq extends Value {
+import es.uned.lsi.eped.DataStructures.List;
 
+public class ValueSeq extends Value {
+	/* Atributo que guarda el valor numérico */
+	private List<Integer> listaEnteros;
+	
 	/* Constructor: recibe un String con el valor numérico */
 	public ValueSeq(String s) {
+		listaEnteros= new List<Integer>();
+		fragmentarNumero(s,listaEnteros);
 	}
 	
 	/* Método que transforma el valor numérico en un String */
@@ -13,6 +19,7 @@ public class ValueSeq extends Value {
 
 	/* Método que modifica el valor numérico llamante, sumándole el valor numérico parámetro */
 	public void addValue(Value n) {
+		Integer.
 	}
 
 	/* Método que modifica el valor numérico llamante, restándole el valor numérico parámetro */
@@ -39,4 +46,17 @@ public class ValueSeq extends Value {
 		return false;
 	}
 
+	private void fragmentarNumero(String s, List<Integer> bloquesEnteros/*,int digitosXbloque*/) {
+		int longitud = s.length();
+		int tamanoTipoEntero=9;
+		
+		int endIndex =longitud+1;
+		int beginIndex=endIndex-tamanoTipoEntero;
+		int pos=0;
+		while(beginIndex>0) {
+			bloquesEnteros.insert(pos, Integer.parseInt(s.substring(beginIndex, endIndex)));
+			endIndex=beginIndex;
+			beginIndex-=tamanoTipoEntero;
+		}
+	}
 }
