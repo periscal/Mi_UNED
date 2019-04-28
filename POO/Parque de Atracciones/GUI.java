464df1760f5a8c528d04a65c13c1ff1cfc0a94b1
periscal <juan.periscal88@gmail.com>
2019-04-28 15:57:31 +0200

    Creacion GUI para POO

---
+++ POO/Parque de Atracciones/GUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+/**
+ * Write a description of class GUI here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GUI extends JFrame{
+    
+}
--- POO/Parque de Atracciones/parque.java
+++ POO/Parque de Atracciones/parque.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
+import javax.swing.JFrame;
 /**
  * Write a description of class parque here.
  * 
@@ -10,7 +11,7 @@ public class parque
 {   
     public static Map<String, Trabajador> trabajdores   = new HashMap<>();
     public static Map<String, Atraccion> atracciones    = new HashMap<>();
-    
+    public static JFrame gui;
     
     public static void main(){
         // Familia modelo
@@ -18,5 +19,13 @@ public class parque
         Turista t2 = new Adulto("Maria","Sanchez","Lopez","02/02/1978");
         Turista t3 = new Nino("Juan", "Garcia","Sanchez","03/03/2008");
         Turista t4 = new Nino("Ana", "Garcia","Sanchez","04/04/2009" );
+        
+        //Solo Adulto
+        
+        //Solo Senior
+        
+        //GUI
+        gui = new GUI();
+        
     }
 }
